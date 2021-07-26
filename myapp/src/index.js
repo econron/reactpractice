2faeeace84f494d0a58a125ce51f68ca0c51f285
econron <aroneconbusi5544@gmail.com>
2021-07-26 15:13:58 +0900

    ガイド読み始め
    https://ja.reactjs.org/docs/introducing-jsx.html

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,88 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+function formatName(user){
+    return user.firstName + ' ' + user.lastName;
+}
 
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={ () => { this.props.onclick() }}
-        >
-          {this.props.val}
-        </button>
-      );
-    }
-  }
-  
-  class Board extends React.Component {
-     
-    constructor(props) {
-        super(props);
-        this.state = {
-          squares: Array(9).fill(null),
-        };
-      }
+const user = {
+    'firstName': 'Okuyama',
+    'lastName': 'Aron'
+};
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
-      }
+// 関数の値を埋め込むには{}を利用する
+const element = (
+    <h1>Hello, {formatName(user)}</h1>
+);
 
-    renderSquare(i) {
-      return <Square 
-      val={this.state.squares[i]} 
-      onclick={() => this.handleClick(i)}
-      />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
