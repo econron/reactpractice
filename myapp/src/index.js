f8eab6ba2ec41ff1edb5ff6cff9f3bd276156df0
econron <aroneconbusi5544@gmail.com>
2021-07-27 15:24:15 +0900

    リストとkey
    keyは呼び出し元ではなく呼び出し先で保持するべき

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,40 +1,23 @@
-import React from 'react';
+import React, { useDebugValue } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
+function ListItem(props) {
+    return <li>{props.value}</li>
+};
 
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (<div>
-            <h1>Hello, world!</h1>
-            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-          </div>);
-    }
-  }
-  
-  function tick() {
-    ReactDOM.render(
-      <Clock />,
-      document.getElementById('root')
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) => <ListItem key = {number.toString()} value = {number} />);
+    return(
+        <ul>
+            {listItems}
+        </ul>
     );
-  }
-  
-  setInterval(tick, 1000);
+};
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
