a5b7c4ca6b17700fff51bfa0526f872ec3e28231
econron <aroneconbusi5544@gmail.com>
2021-07-28 14:53:15 +0900

    条件付きレンダーその１
    普通にif文を利用してレンダーするコンポーネントを指定している。

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,22 +2,69 @@ import React, { useDebugValue } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function ListItem(props) {
-    return <li>{props.value}</li>
-};
+function UserGreeting(props) {
+    return <h1>Welcome back</h1>;
+}
 
-function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) => <ListItem key = {number.toString()} value = {number} />);
+function GuestGreeting(props) {
+    return <h1>Please Sign Up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if(isLoggedIn) {
+        return <UserGreeting />;
+    } else {
+        return <GuestGreeting />;
+    }
+}
+
+function LoginButton(props) {
     return(
-        <ul>
-            {listItems}
-        </ul>
+        <button onClick={props.onClick}>Login</button>
     );
-};
+}
 
-const numbers = [1, 2, 3, 4, 5];
+function LogoutButton(props) {
+    return(
+        <button onClick={props.onClick}>Logout</button>
+    );
+}
+
+class LoginControll extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn : false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn : true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn : false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if(isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this,this.handleLoginClick} />;
+        }
+
+        return(
+            <div>
+                <Greeting isLoggedIn = {isLoggedIn}/>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
-  document.getElementById('root')
+    <LoginControll />,
+    document.getElementById('root')
 );
