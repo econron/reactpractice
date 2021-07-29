ce7c939266f40412db55b764b67e035798aac610
econron <aroneconbusi5544@gmail.com>
2021-07-29 14:51:18 +0900

    フォームその１
    input内の値を常にhandleChangeで操作する。
    handleChangeはstateを更新する。
    input type=textの箇所でvalueとしてstateを参照するよう命令しているので、入力された値は常にstateを参照する。

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,23 +2,37 @@ import React, { useDebugValue } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function MailBox(props) {
-    const unreadMessages = props.unreadMessages;
-    return (
-        <div>
-            <h1>Hello</h1>
-            {
-                unreadMessages.length > 0 &&
-                <h2>
-                    You have {unreadMessages.length} unread messages.
-                </h2>
-            }
-        </div>
-    );
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value : ''};
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        alert('submit');
+        event.preventDefault();
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" name="submit" value="送信！" />
+            </form>
+        );
+    }
 }
 
-const unreadMessages = ['read1', 'read this!', 'Hey!!!'];
 ReactDOM.render(
-    <MailBox unreadMessages = {unreadMessages} />,
+    <NameForm />,
     document.getElementById('root')
 );
