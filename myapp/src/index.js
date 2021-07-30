bcd56af93bc8b479b83522faab8ca77d5c401a32
econron <aroneconbusi5544@gmail.com>
2021-07-30 14:38:04 +0900

    フォーム２
    コンポーネントを2つレンダーしようとしたらエラーになった。
    二つレンダーはダメなのか？

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,37 +2,66 @@ import React, { useDebugValue } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class NameForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value : ''};
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+// class NameForm extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {value : ''};
+//         this.handleChange = this.handleChange.bind(this)
+//         this.handleSubmit = this.handleSubmit.bind(this)
+//     }
 
+//     handleChange(event) {
+//         this.setState({value: event.target.value});
+//     }
+
+//     handleSubmit(event){
+//         alert('submit');
+//         event.preventDefault();
+//     }
+
+//     render() {
+//         return(
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>
+//                     Name:
+//                     <input type="text" value={this.state.value} onChange={this.handleChange} />
+//                 </label>
+//                 <input type="submit" name="submit" value="送信！" />
+//             </form>
+//         );
+//     }
+// }
+
+class EssayForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: "default"
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
     handleChange(event) {
         this.setState({value: event.target.value});
     }
-
-    handleSubmit(event){
-        alert('submit');
+    handleSubmit(event) {
         event.preventDefault();
     }
-
     render() {
-        return(
+        return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    Essay
+                    <textarea value={this.state.value} onChange={this.handleChange} />
                 </label>
-                <input type="submit" name="submit" value="送信！" />
+                <input type="submit" value="Submit" />
             </form>
         );
     }
 }
 
 ReactDOM.render(
-    <NameForm />,
+    // <NameForm />,
+    <EssayForm />,
     document.getElementById('root')
 );
